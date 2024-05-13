9ee0510f13763ce54a683e18cc5d27b6f94d079c
yamao <marina.yamao@rakus-partners.co.jp>
2024-05-13 15:36:47 +0900

    AdministratorClass

---
+++ src/main/java/com/example/domain/Administrator.java
@@ -0,0 +1,61 @@
+package com.example.domain;
+
+public class Administrator {
+
+    /**administrators テーブルのID */
+    private Integer id;
+
+    /**administrators テーブルの名前 */
+    private String name;
+
+    /**administrators テーブルのメールアドレス */
+    private String mailAddress;
+    
+    /**administrators テーブルのパスワード */
+    private String password;
+
+    public Administrator(Integer id, String name, String mailAddress, String password) {
+        this.id = id;
+        this.name = name;
+        this.mailAddress = mailAddress;
+        this.password = password;
+    }
+
+
+    public Administrator() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "Administrator [id=" + id + ", name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
+                + "]";
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getMailAddress() {
+        return mailAddress;
+    }
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+
+}
