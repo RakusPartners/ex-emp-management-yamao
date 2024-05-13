1ee7740dae97a1ffe863a2593ee517b9c1c49ac2
yamao <marina.yamao@rakus-partners.co.jp>
2024-05-13 17:25:40 +0900

    ToAdministratorService/addmethod

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 @Service
@@ -12,5 +13,10 @@ public class AdministratorService {
 
     @Autowired
     private AdministratorRepository repository;
+
+    /** 管理者情報を挿⼊する。*/
+    public void insert(Administrator administrator){
+        repository.insert(administrator);
+    }
 
 }
