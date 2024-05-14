b1462ad702aa18ddb345d26f8b901f05884aa5c9
yamao <marina.yamao@rakus-partners.co.jp>
2024-05-14 10:00:18 +0900

    LoginFormClass

--- src/main/java/com/example/form/LoginForm.java
+++ src/main/java/com/example/form/LoginForm.java
@@ -1,5 +1,33 @@
 package com.example.form;
 
+/**
+ * LoginFormクラスの定義
+ * @author yamaomarina
+ */
 public class LoginForm {
+
+    /**メールアドレス */
+    private String mailAddress;
+    /**パスワード */
+    private String password;
+    
+    @Override
+    public String toString() {
+        return "LoginForm [mailAddress=" + mailAddress + ", password=" + password + "]";
+    }
+    public String getMailAddress() {
+        return mailAddress;
+    }
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
 
 }
