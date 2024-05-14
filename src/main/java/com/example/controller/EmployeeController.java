1fd144bd5ef68f9940ecab176282a069aa261bf2
yamao <marina.yamao@rakus-partners.co.jp>
2024-05-14 12:01:08 +0900

    EmployeeControllerClass

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,5 +1,34 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.service.EmployeeService;
+
+/**
+ * EmployeeControllerクラスの定義
+ * @author yamaomarina
+ */
+
+@Controller
+@RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * 従業員一覧を出力する
+     * @param model
+     * @return
+     */
+    @GetMapping("/showList")
+    public String showList(Model model){
+        model.addAttribute("employeeList", employeeService.showList());
+        return "employee/list";
+    }
 
 }
